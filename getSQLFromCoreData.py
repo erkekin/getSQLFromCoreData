ca01e226f4d97cada9bb80465d6cb2384de3cc90
erk ekin <erkekin@gmail.com>
2013-12-31 14:12:33 +0200

    multiple entity support

--- getSQLFromCoreData.py
+++ getSQLFromCoreData.py
@@ -1,49 +1,81 @@
+# Copyright (c) 2013 erkekin <erkekin at gmail.com> www.erkekin.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
 import sys
 import os
-from xml.dom.minidom import parseString
+
 from xml.dom import minidom
 
 if sys.argv[1].find("xcdatamodeld") != -1:
-	path = "%s/%s.xcdatamodel/contents" % (sys.argv[1],os.path.splitext(os.path.basename(sys.argv[1]))[0])
+    path = "%s/%s.xcdatamodel/contents" % (sys.argv[1], os.path.splitext(os.path.basename(sys.argv[1]))[0])
 else:
-	path = "%s/contents" % (sys.argv[1])
-#print path
+    path = "%s/contents" % (sys.argv[1])
+
 xmldoc = minidom.parse(path)
-itemlist = xmldoc.getElementsByTagName('attribute') 
-entity = xmldoc.getElementsByTagName('entity') 
 
-#CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20)
-string  = 'CREATE TABLE '
-string += entity[0].attributes['name'].value
-string += ' ('
+entity = xmldoc.getElementsByTagName('entity')
 
-for s in itemlist :
-	#print s.attributes['attributeType'].value
-	
-	string += s.attributes['name'].value
-	if s.attributes['attributeType'].value == 'Float':
-		
-		string += ' float'
-	elif 	s.attributes['attributeType'].value == 'Boolean':
-		string += ' boolean'
-	elif 	s.attributes['attributeType'].value == 'Integer 16':
-		string += ' smallint'
-	elif 	s.attributes['attributeType'].value == 'Integer 32':
-		string += ' int'
-	elif 	s.attributes['attributeType'].value == 'Integer 64':
-		string += ' bigint'
-	elif 	s.attributes['attributeType'].value == 'Decimal':
-		string += ' decimal'
-	elif 	s.attributes['attributeType'].value == 'Double':
-		string += ' double'
-	elif 	s.attributes['attributeType'].value == 'Binary':
-		string += ' binary'
-	elif 	s.attributes['attributeType'].value == 'Transformable':
-		string += ' blob'
-		
-	if s != itemlist[len(itemlist)-1]:
-		string += ','
-string += ')'	
+string = ''
 
-print string
-   # print s.attributes['name'].value, s.attributes['attributeType'].value
+for tablename in entity:
+
+    columns = tablename.getElementsByTagName('attribute')
+    string += 'CREATE TABLE '
+    string += tablename.attributes['name'].value
+    string += ' ('
+
+    for column in columns:
+
+        string += column.attributes['name'].value
+
+        if column.attributes['attributeType'].value == 'Float':
+            string += ' float'
+        elif column.attributes['attributeType'].value == 'Boolean':
+            string += ' boolean'
+        elif column.attributes['attributeType'].value == 'Integer 16':
+            string += ' smallint'
+        elif column.attributes['attributeType'].value == 'Integer 32':
+            string += ' int'
+        elif column.attributes['attributeType'].value == 'Integer 64':
+            string += ' bigint'
+        elif column.attributes['attributeType'].value == 'Decimal':
+            string += ' decimal'
+        elif column.attributes['attributeType'].value == 'Double':
+            string += ' double'
+        elif column.attributes['attributeType'].value == 'Binary':
+            string += ' binary'
+        elif column.attributes['attributeType'].value == 'Transformable':
+            string += ' blob'
+        elif column.attributes['attributeType'].value == 'Date':
+            string += ' date'
+        elif column.attributes['attributeType'].value == 'String':
+            string += ' varchar(250)'
+
+        if columns[len(columns)-1] != column:
+            string += ','
+
+    string += ');\n'
+
+fo = open("create.sql", "wb")
+
+fo.write(string)
+
+fo.close()
